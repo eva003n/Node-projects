757c1f5d231f89db4418ba743ec608d08ce58c68
Evan ngugi wambui <evanngugi547@gmail.com>
2026-02-16 22:10:42 +0300

    seperate http handling from server

--- Personal-blog/src/index.js
+++ Personal-blog/src/index.js
@@ -1,37 +1,6 @@
-import express from "express"
-
-const app = express();
+import { app } from "./app.js";
 
 const port = 8000;
-
-// set ejs as the template engine
-app.set("view engine", "ejs");
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-
-//serve static files
-app.use(express.static("public"))
-
-import path from "path";
-import { fileURLToPath } from "url";
-const __fileName = fileURLToPath(import.meta.url)
-const __dirName = path.dirname(__fileName)
-
-// optional: specify views folder (default = ./views) but express does it automatically
-app.set("views", path.resolve(__dirName, "./views"))
-
-import guestRouter from "./routes/guest.routes.js"
-import adminRouter from "./routes/admin.routes.js"
-
-// public rotes
-app.use("/", guestRouter);
-// protected routes
-app.use("/admin", adminRouter )
-
-app.use("/{*splat}", (req, res) => {
-    res.render("notfound");
-})
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
